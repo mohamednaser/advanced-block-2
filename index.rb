ed1b01f68983e0ce28063a565de912d14da61c15
Mohamed Naser <nc_m.naser@hotmail.com>
2019-10-28 18:24:26 +0200

    add my_all function

--- index.rb
+++ index.rb
@@ -9,10 +9,44 @@ module Enumerable
       i += 1
     end
   end
+
+  def my_each_with_index
+    i = 0
+    while i < size
+      yield(self[i], i)
+      i += 1
+    end
+  end
+
+  def my_select
+    new_array = []
+
+    my_each { |element| new_array << element if yield(element) }
+
+    new_array
+  end
+
+  def my_all
+    my_each { |element| return false unless yield(element) == true }
+
+    true
+  end
 end
 
 # tests
 
-# my_each test
-p [1, 2, 3, 4].respond_to?(:my_each) # check response to function name
-[1, 2, 3, 4, 5, 6].my_each { |x| p x } # test function work
+# my_each
+# p [1, 2, 3, 4].respond_to?(:my_each) # check response to function name
+# [1, 2, 3, 4, 5, 6].my_each { |x| p x } # test function work
+
+# my_each_with_index
+# p [1, 2, 3, 4].respond_to?(:my_each_with_index) # check response to function name
+# p [1,2,3,4,5,6,7,8].my_each_with_index{|element , index| p "add element #{element} at index #{index}"}
+
+# my_select
+# p [1, 2, 3, 4].respond_to?(:my_select) # check response to function name
+# p [1, 2, 3, 4, 5, 6, 7, 8].my_select { |element| element > 5 }
+
+# my_all
+# p [1, 2, 3, 4].respond_to?(:my_all) # check response to function name
+# p [1, 2, 3, 4, 5, 6, 7, 8].my_all { |element| element >= 1 } # test my_all function
