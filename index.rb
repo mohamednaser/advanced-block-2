34a8b09a4d9583143c6d02c0cbcbadd357fbc77b
Mohamed Naser <nc_m.naser@hotmail.com>
2019-11-04 11:50:46 +0200

    fix my_count function issues

--- index.rb
+++ index.rb
@@ -3,6 +3,8 @@
 module Enumerable
   # Custom Enumberable Functions
   def my_each
+    return to_enum :my_each unless block_given?
+
     i = 0
     while i < size
       yield(self[i])
@@ -11,6 +13,8 @@ module Enumerable
   end
 
   def my_each_with_index
+    return to_enum :my_each unless block_given?
+
     i = 0
     while i < size
       yield(self[i], i)
@@ -19,6 +23,8 @@ module Enumerable
   end
 
   def my_select
+    return to_enum :my_each unless block_given?
+
     new_array = []
 
     my_each { |element| new_array << element if yield(element) }
@@ -27,6 +33,8 @@ module Enumerable
   end
 
   def my_all
+    return to_enum :my_each unless block_given?
+
     my_each { |element| return false unless yield(element) == true }
 
     true
@@ -42,11 +50,23 @@ module Enumerable
     true
   end
 
-  def my_count
+  def my_count(param = nil)
+    size = 0
+
+    if block_given?
+      my_each { |element| size += yield(element) ? 1 : 0 }
+    elsif param.nil?
+      size = self.size
+    else
+      my_each { |element| size += element == param ? 1 : 0 }
+    end
+
     size
   end
 
   def my_map
+    return to_enum :my_map unless block_given?
+
     i = 0
     array2 = []
     while i < size
@@ -61,16 +81,18 @@ module Enumerable
   end
 
   def my_inject(initial_value_or_symbol = nil)
-    if block_given?
-      result = 0
+    p self
+    p initial_value_or_symbol.is_a? Symbol
+    result = 1
 
-      result = initial_value_or_symbol unless initial_value_or_symbol.nil?
+    if block_given?
+      # result = initial_value_or_symbol unless initial_value_or_symbol.nil?
 
       my_each { |element| result = yield(result, element) }
 
       result
     else
-      my_each { |element| element.Method(initial_value_or_symbol) }
+      my_each { |element| result = element.send(initial_value_or_symbol) }
 
     end
   end
@@ -81,49 +103,3 @@ module Enumerable
     result
   end
 end
-
-# tests
-
-# my_each
-# p [1, 2, 3, 4].respond_to?(:my_each) # check response to function name
-# [1, 2, 3, 4, 5, 6].my_each { |x| p x } # test function work
-
-# my_each_with_index
-# p [1, 2, 3, 4].respond_to?(:my_each_with_index) # check response to function name
-# p [1,2,3,4,5,6,7,8].my_each_with_index{|element , index| p "add element #{element} at index #{index}"}
-
-# my_select
-# p [1, 2, 3, 4].respond_to?(:my_select) # check response to function name
-# p [1, 2, 3, 4, 5, 6, 7, 8].my_select { |element| element > 5 }
-
-# my_all
-# p [1, 2, 3, 4].respond_to?(:my_all) # check response to function name
-# p [1, 2, 3, 4, 5, 6, 7, 8].my_all { |element| element >= 1 } # test my_all function
-
-# my_any
-# p [1, 2, 3, 4].respond_to?(:my_any) # check response to function name
-# p [1, 2, 3, 4, 5, 6, 7, 8].my_any { |element| element >= 8 } # test my_any function
-
-# my_none
-# p [1, 2, 3, 4].respond_to?(:my_none) # check response to function name
-# p [1, 2, 3, 4, 5, 6, 7, 8].my_none { |element| element > 6 } # test my_none function
-
-# my_count
-# p [1, 2, 3, 4].respond_to?(:my_count) # check response to function name
-# p [1, 2, 3, 4, 5, 6, 7, 8].my_count { |element| element > 6 } # test my_count function
-
-# my_map
-# p [1, 2, 3, 4].respond_to?(:my_map) # check response to function name
-# p [1, 2, 3, 4, 5, 6, 7, 8].my_map { |element| element + 6 } # test my_map function
-
-# my_inject
-# p [1, 2, 3, 4].respond_to?(:my_inject) # check response to function name
-# p [1, 2, 3, 4].my_inject { |sum, _element| sum + _element } # test my_inject function
-# p [1, 2, 3, 4].my_inject(8) { |sum, element| sum + element } # test my_inject function with inital value
-# p [1, 2, 3, 4].my_inject(:+) # test my_inject function with symbol
-
-# my_map function with proc parmater
-# sum_proc = proc do |element|
-#   element + 6
-# end
-# p [1, 2, 3, 4, 5, 6, 7, 8].my_map(&sum_proc) # test my_map function
